ea8e28fbf24e57c1e20b32f0be9846ac99d4f24a
michellevanderlinde <2747268@gmail.com>
2024-06-27 14:45:00 +0200

    Implement mysql database in project

--- src/main/resources/import.sql
+++
@@ -1,9 +0,0 @@
-
-insert into account (firstName, lastName, address, IBAN, currency, balance, creationDate) values
-("Joe", "Stale", "52 Goudastraat Almere", "ONB2510036502", "EUR", 5600.00, '2024-06-26 07:22:42.017');
-
-insert into account (firstName, lastName, address, IBAN, currency, balance, creationDate) values
-("Chase", "Cruz", "12 Broek Utrecht", "ONB9650027420", "EUR", 1200.00, '2024-05-26 11:33:14.012');
-
-insert into account (firstName, lastName, address, IBAN, currency, balance, creationDate) values
-("Rob", "Schuurman", "302 Buitenloopstraat Heemskerk", "ONB9650205512", "EUR", 10500.00, '2024-05-12 14:58:00.035');
