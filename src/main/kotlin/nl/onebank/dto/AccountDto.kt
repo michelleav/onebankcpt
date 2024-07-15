31632b72e99e6cbbc9cfb0a0bcd797bb5ba1bdce
michellevanderlinde <2747268@gmail.com>
2024-07-15 14:58:42 +0200

    Update AccountDTO and update README.md

--- src/main/kotlin/nl/onebank/dto/AccountDto.kt
+++ src/main/kotlin/nl/onebank/dto/AccountDto.kt
@@ -1,13 +1,12 @@
 package nl.onebank.dto
 
 import nl.onebank.onebankcpt.accountmanagement.entity.Account
-import nl.onebank.onebankcpt.accountmanagement.entity.Transfer
 import java.util.*
 
-class AccountDto (var firstName: String, var lastName: String, var address: String,
-                  var IBAN: String, var currency: String, var balance: Double){
+data class AccountDto (var firstName: String, var lastName: String, var address: String,
+                  var iban: String, var currency: String, var balance: Double){
     fun toEntity(): Account {
-        return Account(null, firstName, lastName, address, IBAN, currency, balance, Date())
+        return Account(null, firstName, lastName, address, iban, currency, balance, Date())
     }
 }
 
